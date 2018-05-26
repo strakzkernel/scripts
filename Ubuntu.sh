4af8be064298ba29e01b94821c76f084d9e689ab
Revanth Strakz <39440107+strakzkernel@users.noreply.github.com>
2018-05-27 01:06:56 +0530

    Update Ubuntu.sh

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,24 +1,24 @@
 
 sudo apt-get update -y
 sudo add-apt-repository --yes ppa:webupd8team/java
-sudo apt-get install oracle-java9-installer
-sudo apt install oracle-java9-set-default
-sudo apt-get install libncurses5-dev
-sudo apt-get install git-core gnupg flex bison gperf
+sudo apt-get -y install oracle-java9-installer
+sudo apt -y install oracle-java9-set-default
+sudo apt-get -y install libncurses5-dev
+sudo apt-get -y install git-core gnupg flex bison gperf
  
-sudo apt-get install build-essential
-sudo apt-get install zip
-sudo apt-get install curl 
-sudo apt-get install libc6-dev
-sudo apt-get install git git-svn libncurses5-dev:i386 
-sudo apt-get install x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386
-sudo apt-get install libgl1-mesa-glx:i386 libgl1-mesa-dev 
-sudo apt-get install g++-multilib tofrodos python-markdown
-sudo apt-get install libxml2-utils xsltproc zlib1g-dev:i386
+sudo apt-get -y install build-essential
+sudo apt-get -y install zip
+sudo apt-get -y install curl 
+sudo apt-get -y install libc6-dev
+sudo apt-get -y install git git-svn libncurses5-dev:i386 
+sudo apt-get -y install x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386
+sudo apt-get -y install libgl1-mesa-glx:i386 libgl1-mesa-dev 
+sudo apt-get -y install g++-multilib tofrodos python-markdown
+sudo apt-get -y install libxml2-utils xsltproc zlib1g-dev:i386
 sudo  ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
-sudo apt-get install -y ccache &&echo 'export PATH="/usr/lib/ccache:$PATH"' | tee -a ~/.bashrc &&source ~/.bashrc && echo $PATH
+sudo apt-get -y install -y ccache &&echo 'export PATH="/usr/lib/ccache:$PATH"' | tee -a ~/.bashrc &&source ~/.bashrc && echo $PATH
 export USE_CCACHE=1
-sudo apt-get install aptitude
+sudo apt-get -y install aptitude
 sudo aptitude -y install build-essential bc binfmt-support libllvm-3.6-ocaml-dev llvm-3.6 llvm-3.6-dev llvm-3.6-runtime cmake automake autogen autoconf autotools-dev libtool shtool python m4 gcc libtool zlib1g-dev
 cd ${HOME}
 PATH=~/bin:$PATH
